835158403b3431b039cfd26baa93cb26b800caae
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-03-19 14:56:13 -0300

    Create Pilha.java

---
+++ Pilha.java
@@ -0,0 +1,42 @@
+public class Pilha {
+
+    public int dados[];
+    public int topo = -1;
+
+    public Pilha(int tamanho) {
+        this.dados = new int[tamanho];
+        this.topo = -1;
+    }
+
+    public void insere(int valor){
+        if(topo < dados.length - 1){
+            topo ++;
+            dados[topo] = valor;
+            System.out.println("Valor inserido: " + valor);
+        }
+        else{
+            System.out.println("Pilha cheia");
+        }
+    }
+
+    public int remover() {
+        if (topo >= 0) {
+            int valorRemovido = dados[topo];
+            topo--;
+            return valorRemovido;
+        } else {
+            System.out.println("Pilha vazia! Não há elementos para remover.");
+            return -1;
+        }
+    }
+
+    public int topo(){
+        if (topo >= 0) {
+            return dados[topo];
+        }
+        else{
+            System.out.println("Pilha vazia");
+            return -1;
+        }
+    }
+}
