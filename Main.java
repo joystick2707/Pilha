8856772d90abcc5c867b57d2db05b173acd4ff7b
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-03-26 20:35:27 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Pilha pilha = new Pilha(5);
+        Pilha pilha = new Pilha();
 
         pilha.insere(10);
         pilha.insere(20);
         pilha.insere(30);
 
         System.out.println("Topo da pilha: " + pilha.topo());
-        
+
+        pilha.remover();
         pilha.remover();
 
-        System.out.println("Topo da pilha após pop: " + pilha.topo());
+        System.out.println("Topo da pilha: " + pilha.topo());
+
+        System.out.println("Pilha está vazia? " + pilha.estaVazia());
+
+        pilha.remover();
+        System.out.println("Pilha está vazia? " + pilha.estaVazia());
     }
 }
